7caeb11d2ef89ce8162c907c70a8ff4f7a461e0d
Satyam Anand <satyamanand135@gmail.com>
2026-01-24 11:47:46 +0530

    Vectors

--- c++_stl.cpp
+++ c++_stl.cpp
@@ -15,5 +15,22 @@ int main() {
         cout << "(" << pr.first << ", " << pr.second << ")\n";
     }
 
+
+        // Vectors
+    vector<int> v = {1, 2, 3};
+    v.push_back(4);
+    v.emplace_back(5);//It is faster than push_back
+
+    vector<pair<int, int>> vec_pairs;
+    vec_pairs.push_back({1, 2});
+    vec_pairs.emplace_back(3, 4);//It is faster than push_back and you don't need to use {}
+    vector<int> v2(5, 10); // Vector of size 5, all initialized to 10 
+
+    vector<int>::iterator i=v.begin();
+    i++;
+    cout << *(i) << "\n"; // Outputs 2
+
+    vector<int>::iterator j=v.end(); // This will not point to the last element, but one past the last element
+
     return 0;
 }
