33862b461c48edaa27e5feea5481820cd5c6dca2
Satyam Anand <satyamanand135@gmail.com>
2026-01-24 11:08:55 +0530

    Time And Space Complexity

---
+++ time_and_space_complexity.cpp
@@ -0,0 +1,25 @@
+// Time Complexity
+
+// Q1. for(int i=0; i<n; i++
+// ) {
+//     for(int j=0; j<n; j++) {
+//         // some O(1) operations
+//     }
+// }
+
+// O(n^2)[Time Complexity]
+
+
+// Q2. for(int i=0; i<n; i++
+// ) {for(int j=0; j<i; j++) {
+//         // some O(1) operations
+//     }
+// }
+
+// O(n^2)[Time Complexity]  
+
+// Space Complexity = Auxiliary Space + Input Space
+// Never change the input space. 
+
+
+
